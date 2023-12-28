6d41a5b6764486ed350e201fc587845f71d2d4ec
leoshush <135488598+leoshush@users.noreply.github.com>
2023-12-28 06:54:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert() we are using nodejs , not browser
+
+console.log(3+3)
+console.log("Hitesh")
+
+//number=> 2 to power 53
+//bigint
+//string
+//boolean
+//null 
+//undefined
+//symbol => unique
+
+// object
+
+console.log(typeof (3))
+console.log(typeof null)
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("Hitesh")
+const accountId = 1445
+let accountEmail = "hitesh@google.com"
+var accountCity = "12345"
+accountCity="Jaipur"
+
+
+// accountId = 2
+console.log(accountId)
+accountEmail = "hc@c.com"
+accountPassword = "124236"
+console.table([accountCity,accountEmail,accountId,accountPassword])
+
+// Prefer not to use var bacause of block scope and  functional scope 
