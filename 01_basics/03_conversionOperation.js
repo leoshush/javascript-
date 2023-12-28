94cd734cc876140aa700f2255ef10719ca2ad642
leoshush <135488598+leoshush@users.noreply.github.com>
2023-12-28 07:26:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33abc"
+console.log(typeof score)
+
+// let valueinnumber = Number(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber);
+
+let isLoggedIn = 1
+let booleanisloggedIn = Boolean(isLoggedIn)
+// console.log(booleanisloggedIn)
+
+// "" => false
+// "ashish" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber);
+
