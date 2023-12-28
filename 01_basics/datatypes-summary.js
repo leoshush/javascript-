27ac477127f10aef6ba9b4342efc3773a48e3301
leoshush <135488598+leoshush@users.noreply.github.com>
2023-12-28 07:57:45 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+// Primitive 
+
+// 7 types : String,Number,Boolean,Null,undefined,Symbol,BigInt
+
+const score  = 100.2
+const isLoggedIn = false;
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherid =  Symbol('123')
+// console.log(id ===  anotherid);
+
+const bigNumber = 2343642768857698n
+// reference type (Non primitive)
+
+//Array , Objects , Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+let myobj = {
+    name: "ashish",
+    age : 22,
+}
+
+const myfunc = function() {
+ console.log("hello ashish")
+}
+
+// console.log(typeof myfunc)
