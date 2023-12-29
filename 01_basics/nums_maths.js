9b8f1c0f5d0c2a5a69ebf06773b2d190b30577ac
leoshush <135488598+leoshush@users.noreply.github.com>
2023-12-29 06:31:17 +0000

    Math and num

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,24 @@
+const score = 400 
+// console.log(score)
+const balance = new Number(100)
+
+
+// console.log(typeof balance) -> object 
+
+// console.log(balance.toString())
+// console.log(typeof score.toString()) -> string
+// console.log(typeof score) -> number
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ************************************ Maths *******************************
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+console.log((Math.random())*5);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1) )+ min)
