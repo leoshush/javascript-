e02ee0dc60ef2f3531596c6af0cb81b7b403a2dc
leoshush <135488598+leoshush@users.noreply.github.com>
2023-12-29 06:09:20 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,24 @@
+const name = "hitesh"
+const repoCount = 50
+console.log(name + repoCount + "Value")
+
+console.log(`Hello ${name} is very hot`)
+
+const gamename = new String("ashish")
+// console.log( typeof gamename)
+
+// console.log(gamename.__proto__)
+// console.log(gamename.length)
+
+const newString = gamename.substring(0,4);
+console.log(newString)
+
+const anotherString = gamename.slice(0,-4)
+console.log(anotherString)
+
+const newStringOne = " hitesh "
+console.log(newStringOne.trim())
+
+const url = "https://google@32.com"
+
+console.log( url.replace("@32",""));
