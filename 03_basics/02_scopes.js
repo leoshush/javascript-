c6cee9549771f2e9c1d918f5fbbd8bc4813321b4
leoshush <135488598+leoshush@users.noreply.github.com>
2024-01-02 05:21:42 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,46 @@
+if (true)
+{
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c); -> this value will be printed due to var issue
+
+function one(){
+    const username = "ashish"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    console.log(website);
+    // two()
+}
+
+// one()
+
+// if (true){
+//     const username = "ashish"
+//     if(username === "ashish")
+//      {
+//         const website = "youtube"
+//         // console.log(username + website);
+//      }
+    //  console.log(website)
+// }
+console.log(addone(5))
+function addone(num){
+    return num  +1 
+}
+
+// addone(5)
+
+addTwo(5)
+const addTwo = function(num) {
+    return num + 2
+}
+
+
+
