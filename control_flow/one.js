dbd4352241d279687c27086d1dd2a25a41739711
leoshush <135488598+leoshush@users.noreply.github.com>
2024-01-19 14:21:15 +0000

    if_else

---
+++ control_flow/one.js
@@ -0,0 +1,59 @@
+//if
+
+// if(2 === "2")
+// {
+//   console.log("ece");
+// }
+// else
+// {
+//     console.log("not equal")
+// }
+// if(temperature===41)
+// {
+//   console.log("less than 50")
+// }
+// else
+// {
+//   console.log("temperature is greater than 50")
+// }
+// <,>,<=,>=,==,!=,===
+
+// const score = 300
+// if (score>200)
+// {  const power = "fly"
+//    console.log(`User power: ${power}`);
+// }
+
+const balance =1000
+// if(balance>500)
+// {
+//   console.log("test");
+// }
+
+// if(balance<500)
+// {
+//    console.log("less than");
+// }
+// else if(balance<750)
+// {
+//   console.log("less than 750");
+// }
+// else
+// {
+//   console.log("less tham 1200")
+// }
+
+// const userLoggedIn=true 
+// const debitCard = true
+// if (userLoggedIn && debitCard)
+// {
+//   console.log("allow user to buy course");
+// }
+const loggedinfromgoogle = false;
+const loogedinfromemail = true;
+if(loggedinfromgoogle | loogedinfromemail)
+{
+   console.log("user logged in");
+}
+
+
